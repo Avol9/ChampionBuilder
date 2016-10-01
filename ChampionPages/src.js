3283ef111519e6fab4ea907db5c3442b0d36febf
Arun Arjunakani <arunarjunakani@gmail.com>
2016-10-01 09:15:59 -0400

    Got the update stats method to work

--- ChampionPages/src.js
+++ ChampionPages/src.js
@@ -1,14 +1,14 @@
 var kayleRead = angular.module("kayleJson", []);
-kayleRead.controller("kayleCtrl", function ($scope, $http) {
-    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/10?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function (response) {
+kayleRead.controller("kayleCtrl", function($scope, $http) {
+    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/10?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function(response) {
         $scope.myData = response.data.stats;
         console.log($scope.myData);
     });
 });
 
 var kledRead = angular.module("kledJson", []);
-kledRead.controller("kledCtrl", function ($scope, $http) {
-    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/240?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function (response) {
+kledRead.controller("kledCtrl", function($scope, $http) {
+    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/240?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function(response) {
         $scope.info = response;
         $scope.myData = $scope.info.data.stats;
 
@@ -16,9 +16,9 @@ kledRead.controller("kledCtrl", function ($scope, $http) {
 });
 
 var khazixRead = angular.module("khazixJson", []);
-khazixRead.controller("khazixCtrl", function ($scope, $http) {
-    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/121?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function (response) {
-        $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?itemListData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function (responseItem) {
+khazixRead.controller("khazixCtrl", function($scope, $http) {
+    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/121?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function(response) {
+        $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?itemListData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function(responseItem) {
             $scope.info = response;
             $scope.myData = response.data.stats;
             $scope.itemData = responseItem.data.data;
@@ -28,11 +28,14 @@ khazixRead.controller("khazixCtrl", function ($scope, $http) {
                 healthRegen: 0,
                 attackDamage: 0,
                 abilityPower: 0,
-                attackSpeed: 0
+                attackSpeed: 0,
+                magicResist: 0,
+                movementSpeed: 0,
+                cdr: 0
             }
             var itemBuild = [-1, -1, -1, -1, -1, -1];
 
-            $scope.checkItem = function (id) {
+            $scope.checkItem = function(id) {
                 if (id == 3077 || id == 3074 || id == 3748) {
                     if (!$scope.stats.isMelee) {
                         return false;
@@ -46,7 +49,7 @@ khazixRead.controller("khazixCtrl", function ($scope, $http) {
                 return true;
             }
 
-            $scope.addItem = function (id) {
+            $scope.addItem = function(id) {
                 if ($scope.checkItem(id)) {
                     console.log("addItem() works");
                     for (var i = 0; i < itemBuild.length; i++) {
@@ -59,20 +62,37 @@ khazixRead.controller("khazixCtrl", function ($scope, $http) {
                 }
             }
 
-            $scope.removeItem = function (id) {
+            $scope.removeItem = function(id) {
                 itemBuild[id] = -1;
                 $scope.updateStats();
             }
 
-            $scope.updateStats = function () {
+            //This method updates the stats based on the item array
+            $scope.updateStats = function() {
+                //Nullifies all the stats
                 $scope.stats.health = 0;
                 $scope.stats.healthRegen = 0;
                 $scope.stats.attackDamage = 0;
                 $scope.stats.attackSpeed = 0;
                 $scope.stats.abilityPower = 0;
-                for (var i = 0; i < itemBuild.length; i++) {
-                    if (itemBuild != -1) {
+                $scope.stats.magicResist = 0;
+                $scope.stats.cdr = 0;
+                $scope.stats.movementSpeed = 0;
 
+                //Cycles through the loop
+                for (var i = 0; i < itemBuild.length; i++) {
+                    if (itemBuild[i] != -1) {
+                        if ($scope.itemData[itemBuild[i]].stats.FlatHPPoolMod != null) {
+                            $scope.stats.health += $scope.itemData[itemBuild[i]].stats.FlatHPPoolMod;
+                        }
+
+                        if ($scope.itemData[itemBuild[i]].stats.FlatHPRegenPoolMod != null) {
+                            $scope.stats.healthRegen += $scope.itemData[itemBuild[i]].stats.FlatHPRegenPoolMod;
+                        }
+
+                        if ($scope.itemData[itemBuild[i]].stats.FlatMovementSpeedMod != null) {
+                            $scope.stats.movementSpeed += $scope.itemData[itemBuild[i]].stats.FlatMovementSpeedMod;
+                        }
                     }
                 }
                 console.log(itemBuild);
@@ -86,17 +106,17 @@ khazixRead.controller("khazixCtrl", function ($scope, $http) {
 
 
 var aatroxRead = angular.module("aatroxJson", []);
-aatroxRead.controller("aatroxCtrl", function ($scope, $http) {
-    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/266?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function (response) {
+aatroxRead.controller("aatroxCtrl", function($scope, $http) {
+    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/266?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function(response) {
         $scope.myData = response.data.stats;
         console.log($scope.myData);
     });
 });
 
 var yasuoRead = angular.module("yasuoJson", []);
-yasuoRead.controller("yasuoCtrl", function ($scope, $http) {
-    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/157?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function (response) {
-        $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?itemListData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function (responseItem) {
+yasuoRead.controller("yasuoCtrl", function($scope, $http) {
+    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/157?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function(response) {
+        $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/item?itemListData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function(responseItem) {
             $scope.myData = response.data.stats;
             $scope.itemData = responseItem.data.data;
             console.log($scope.myData);
@@ -106,8 +126,8 @@ yasuoRead.controller("yasuoCtrl", function ($scope, $http) {
 });
 
 var mfRead = angular.module("mfJson", []);
-mfRead.controller("mfCtrl", function ($scope, $http) {
-    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/21?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function (response) {
+mfRead.controller("mfCtrl", function($scope, $http) {
+    $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/21?champData=stats&api_key=ba5c1405-42bb-4e90-bfc7-ee33f98df4bf").then(function(response) {
         $scope.myData = response.data.stats;
         console.log($scope.myData);
     });
